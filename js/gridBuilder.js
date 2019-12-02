79a3924a868f02aff57c42baed8d1d5172eed67e
Yanivba18 <47143949+Yanivba18@users.noreply.github.com>
2019-12-02 23:41:31 +0200

    handling clicks now, eraser removes and stores

--- js/gridBuilder.js
+++ js/gridBuilder.js
@@ -1,212 +1,171 @@
 "use strict";
+(() => {
 
-class gameUI {
-    constructor(totalCols) {
-        this.totalCols = totalCols;
-        this.totalRows = 0;
-        this.groundHeight = Math.floor(Math.random() * 3) + 3;
-        this.groundStartRow = undefined;
-    }
-
-    buildGrid() {
-        let height = $(window).height();
-        let rows = Math.floor(height / 40);
-
-        this.totalRows = rows;
-        let $mainContainer = $('.main-container');
-
-        for (let r = 0; r < rows; r++) {            
-            let rowDiv = document.createElement("div");
-            rowDiv.classList.add("row")
-            for (let c = 0; c < this.totalCols; c++) {
-                let col = new Tile(r, c);
-                rowDiv.appendChild(col.getDiv());
-            }
-            $mainContainer.append($(rowDiv));
+    class GameGrid {
+        constructor(totalCols) {
+            this.totalCols = totalCols;
+            this.totalRows = 0;
+            this.groundHeight = Math.floor(Math.random() * 3) + 3;
+            this.groundStartRow = undefined;
         }
 
-    }
+        buildGrid() {
+            let height = $(window).height();
+            let rows = Math.floor(height / 40);
 
-    createTree() {
-        let treeStartCol = Math.floor(Math.random() * (this.totalCols - 3));
-        let tempRow = this.groundStartRow - 3;
-        while (!hasGroundBelow(tempRow, treeStartCol + 1)) {
-            tempRow++;
-            console.log(tempRow);
-        }
-        let startRow = tempRow - 5;
-        let startCol = treeStartCol;
-        console.log("row: " + startRow + " cols: " + startCol)
-        for (let r = 0; r < 6; r++) {
-            if (r < 3) {
-                for (let c = 0; c < 3; c++) {
-                    let treeLeaf = document.getElementById(`${startRow}X${startCol}`);
-                    treeLeaf.classList.add("treeLeaf")
-                    startCol++;
+            this.totalRows = rows;
+            let $mainContainer = $('.main-container');
+
+            for (let r = 0; r < rows; r++) {
+                let rowDiv = document.createElement("div");
+                rowDiv.classList.add("row")
+                for (let c = 0; c < this.totalCols; c++) {
+                    let col = new Tile(r, c);
+                    rowDiv.appendChild(col.getDiv());
+                    col.bind
                 }
+                $mainContainer.append($(rowDiv));
             }
-            else {
-                let treeTrunk = document.getElementById(`${startRow}X${startCol + 1}`);
-                treeTrunk.classList.add("treeTrunk")
-            }
-            startCol = treeStartCol;
-            startRow++;
-        }
-    }
 
-    createGround() {
-        this.groundStartRow = (this.totalRows - this.groundHeight);
-        for (let r = this.groundStartRow; r < this.totalRows; r++) {
-            for (let c = 0; c < this.totalCols; c++) {
-                let groundDiv = document.getElementById(`${r}X${c}`);
-                groundDiv.classList.add("ground");
-                if (r == (this.totalRows - 1)) {
-                    groundDiv.classList.add("lava");
+        }
+
+        createTree() {
+            let treeStartCol = Math.floor(Math.random() * (this.totalCols - 3));
+            let tempRow = this.groundStartRow - 3;
+            while (!this.hasGroundBelow(tempRow, treeStartCol + 1)) {
+                tempRow++;
+                console.log(tempRow);
+            }
+            let startRow = tempRow - 5;
+            let startCol = treeStartCol;
+            console.log("row: " + startRow + " cols: " + startCol)
+            for (let r = 0; r < 6; r++) {
+                if (r < 3) {
+                    for (let c = 0; c < 3; c++) {
+                        let treeLeaf = document.getElementById(`${startRow}X${startCol}`);
+                        treeLeaf.classList.add("treeLeaf")
+                        startCol++;
+                    }
                 }
+                else {
+                    let treeTrunk = document.getElementById(`${startRow}X${startCol + 1}`);
+                    treeTrunk.classList.add("treeTrunk")
+                }
+                startCol = treeStartCol;
+                startRow++;
             }
         }
-    }
 
-    createHills() {
-        let currentRow = 1;
-        for (let r = (this.groundStartRow - 1); r > this.groundStartRow - 3; r--) {
-            console.log(r)
-            for (let c = 0; c < this.totalCols; c++) {
-                let chance = Math.random();
-                if ((currentRow == 1 || hasGroundBelow(r, c)) && chance > 0.5) {
+        createGround() {
+            this.groundStartRow = (this.totalRows - this.groundHeight);
+            for (let r = this.groundStartRow; r < this.totalRows; r++) {
+                for (let c = 0; c < this.totalCols; c++) {
                     let groundDiv = document.getElementById(`${r}X${c}`);
-                    groundDiv.classList.add("hill");
+                    groundDiv.classList.add("ground");
+                    if (r == (this.totalRows - 1)) {
+                        groundDiv.classList.add("lava");
+                    }
                 }
-
             }
-            currentRow++;
+        }
+
+        createHills() {
+            let currentRow = 1;
+            for (let r = (this.groundStartRow - 1); r > this.groundStartRow - 3; r--) {
+                console.log(r)
+                for (let c = 0; c < this.totalCols; c++) {
+                    let chance = Math.random();
+                    if ((currentRow == 1 || this.hasGroundBelow(r, c)) && chance > 0.5) {
+                        let groundDiv = document.getElementById(`${r}X${c}`);
+                        groundDiv.classList.add("hill");
+                    }
+
+                }
+                currentRow++;
+            }
+        }
+
+        hasGroundBelow(r, c) {
+            let below = document.getElementById(`${r + 1}X${c}`);
+
+            if (below.classList.contains("hill") || below.classList.contains("ground")) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        createToolBar() {
+            console.log("Tool bar not implemented yet")
+            //Need to implement the tool bar with 4 tools here
+            //Each one needs an onclick which changes gameUI.currentAction = chosenAction which is one of: 
+            // 1. "axe"
+            // 2. "shovel"
+            // 3. "pickaxe"
+            // 4. "eraser"
         }
     }
-}
 
-class Tile {
-    constructor(row, col) {
-        this.row = row;
-        this.col = col;
-    }
-    
-    getDiv() {
-        let colDiv = document.createElement("div");
-        colDiv.classList.add("regular-div");
-        colDiv.setAttribute("id", `${this.row}X${this.col}`);
-        return colDiv;
-    }
+    class Tile {
+        constructor(row, col) {
+            this.row = row;
+            this.col = col;
+            this.div = undefined;
+        }
 
-    
-}
-
-
-// let gameUI = {
-//     totalRows: 0,
-//     totalCols: 0,
-//     groundHeight: Math.floor(Math.random() * 3) + 3,
-//     groundStartRow: undefined,
-// }
-
-// gameUI.buildGrid = (cols) => {
-//     gameUI.totalCols = cols;
-//     let height = $(window).height();
-//     let rows = Math.floor(height / 40);
-
-//     gameUI.totalRows = rows;
-//     let $mainContainer = $('.main-container');
-
-//     for (let r = 0; r < rows; r++) {
-//         let rowDiv = document.createElement("div");
-//         rowDiv.classList.add("row")
-//         for (let c = 0; c < cols; c++) {
-//             let colDiv = document.createElement("div");
-//             colDiv.classList.add("regular-div");
-//             colDiv.setAttribute("id", `${r}X${c}`);
-//             rowDiv.appendChild(colDiv);
-//         }
-//         $mainContainer.append($(rowDiv));
-//     }
-// }
-
-
-// gameUI.createTree = () => {
-//     //TODO add check to make sure the tree is just touching the ground, not above not below
-//     let treeStartCol = Math.floor(Math.random() * (gameUI.totalCols - 3));
-//     let tempRow = gameUI.groundStartRow - 3;
-//     while (!gameUI.hasGroundBelow(tempRow, treeStartCol + 1)) {
-//         tempRow++;
-//         console.log(tempRow);
-//     }
-//     let startRow = tempRow - 5;
-//     let startCol = treeStartCol;
-//     console.log("row: " + startRow + " cols: " + startCol)
-//     for (let r = 0; r < 6; r++) {
-//         if (r < 3) {
-//             for (let c = 0; c < 3; c++) {
-//                 let treeLeaf = document.getElementById(`${startRow}X${startCol}`);
-//                 treeLeaf.classList.add("treeLeaf")
-//                 startCol++;
-//             }
-//         }
-//         else {
-//             let treeTrunk = document.getElementById(`${startRow}X${startCol + 1}`);
-//             treeTrunk.classList.add("treeTrunk")
-//         }
-//         startCol = treeStartCol;
-//         startRow++;
-//     }
-// }
-
-// gameUI.createGround = () => {
-//     gameUI.groundStartRow = (gameUI.totalRows - gameUI.groundHeight);
-//     for (let r = gameUI.groundStartRow; r < gameUI.totalRows; r++) {
-//         for (let c = 0; c < gameUI.totalCols; c++) {
-//             let groundDiv = document.getElementById(`${r}X${c}`);
-//             groundDiv.classList.add("ground");
-//             if (r == (gameUI.totalRows - 1)) {
-//                 groundDiv.classList.add("lava");
-//             }
-//         }
-//     }
-// }
-
-// gameUI.createHills = () => {
-//     let currentRow = 1;
-//     for (let r = (gameUI.groundStartRow - 1); r > gameUI.groundStartRow - 3; r--) {
-//         console.log(r)
-//         for (let c = 0; c < gameUI.totalCols; c++) {
-//             let chance = Math.random();
-//             if ((currentRow == 1 || gameUI.hasGroundBelow(r, c)) && chance > 0.5) {
-//                 let groundDiv = document.getElementById(`${r}X${c}`);
-//                 groundDiv.classList.add("hill");
-//             }
-
-//         }
-//         // console.log(chance)
-//         currentRow++;
-//     }
-// }
-
-let hasGroundBelow = (r, c) => {
-    // let leftSide = document.getElementById(`${r}X${c == 0 ? c : c - 1}`);
-    // let rightSide = document.getElementById(`${r}X${c == (gameUI.totalCols - 1) ? c : c + 1}`);
-    let below = document.getElementById(`${r + 1}X${c}`);
-
-    if (below.classList.contains("hill") || below.classList.contains("ground")) {
-        return true;
+        getDiv() {
+            this.div = document.createElement("div");
+            this.div.classList.add("regular-div");
+            this.div.setAttribute("id", `${this.row}X${this.col}`);
+            this.div.addEventListener("click", function (e) { gameUI.handleAction(e) })
+            return this.div;
+        }
     }
 
-}
+    let gameUI = {
+        currentAction: "eraser",
+        minedTiles: {},
+    };
 
-function main() {
-    let newGameUI = new gameUI(40);
-    newGameUI.buildGrid();
-    newGameUI.createGround();
-    // console.log(newGameUI.groundStartRow)
-    // console.log(newGameUI.totalRows)
-    newGameUI.createHills();
-    newGameUI.createTree();
-}
 
-main();
+
+    gameUI.handleAction = (e) => {
+        console.log(gameUI.currentAction + " " + e.target.classList)
+        if (e.target.classList == "regular-div") {
+            console.log("Nothing in this div but sky");
+            return;
+        }
+        let minedTile;
+        switch (gameUI.currentAction) {
+            case "axe":
+                break;
+            case "shovel":
+                break;
+            case "pickaxe":
+                break;
+            case "eraser":
+                minedTile = e.target.classList[1];
+                console.log("Mined tile: " + minedTile);
+                if (gameUI.minedTiles[minedTile]) {
+                    gameUI.minedTiles[minedTile]++;
+                } else {
+                    gameUI.minedTiles[minedTile] = 1;
+                }
+                console.log(gameUI.minedTiles);
+                e.target.classList = "regular-div";
+                break;
+        }
+    };
+
+    function main() {
+        let newGameUI = new GameGrid(40);
+        newGameUI.buildGrid();
+        newGameUI.createGround();
+        // console.log(newGameUI.groundStartRow)
+        // console.log(newGameUI.totalRows)
+        newGameUI.createHills();
+        newGameUI.createTree();
+    }
+
+    main();
+})(window)
