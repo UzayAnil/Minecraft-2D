25aa3df1888a316406349003ce179b24be787e4e
Yanivba18 <47143949+Yanivba18@users.noreply.github.com>
2019-12-02 15:45:53 +0200

    Create tree works, created gameUI obj

--- js/gridBuilder.js
+++ js/gridBuilder.js
@@ -1,21 +1,55 @@
-(() => {    
+(() => {
     
-    function buildGrid() {
-        let width = $(window).width();
+    let gameUI = {
+        totalRows: 0,
+    }
+    
+    gameUI.buildGrid = (cols) => {
         let height = $(window).height();
-        let rows = width / 22;
-        let cols = height / 22;
+        let rows = (height / 40) - 1;
+        gameUI.totalRows = rows;
         let $mainContainer = $('.main-container');
-        for (let i = 0; i < rows; i++) {
+        for (let r = 0; r < rows; r++) {
             let rowDiv = document.createElement("div");
-            for (let x = 0; x < cols; x++) {
+            rowDiv.classList.add("row")
+            for (let c = 0; c < cols; c++) {
                 let colDiv = document.createElement("div");
                 colDiv.classList.add("regular-div");
+                colDiv.setAttribute("id", `${r}X${c}`);
                 rowDiv.appendChild(colDiv);
             }
             $mainContainer.append($(rowDiv));
         }
     }
 
-    buildGrid();
+    
+    gameUI.createTree = (treeStartRow, treeStartCol) => {
+        //TODO add check to make sure the tree is just touching the ground, not above not below
+        
+        startRow = treeStartRow;
+        startCol = treeStartCol;
+        for (let i = 0; i < 6; i++) {
+            if (i < 3) {
+                for (let z = 0; z < 3; z++) {
+                    let treeLeaf = document.getElementById(`${startRow}X${startCol}`);
+                    treeLeaf.classList.add("treeLeaf")
+                    startCol++;
+                }
+            }
+            else {
+                let treeLeaf = document.getElementById(`${startRow}X${startCol + 1}`);
+                treeLeaf.classList.add("treeTrunk")
+            }
+            startCol = treeStartCol;
+            startRow++;
+        }
+    }
+    
+    gameUI.createGround = (groundStartRow) => {
+
+    }
+    
+    gameUI.buildGrid(40);
+    gameUI.createTree(2, 10);
+    console.log(gameUI.totalRows)
 })(window)
