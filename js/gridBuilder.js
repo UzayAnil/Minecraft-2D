4e0adf8decaabb2ac631f821d70a83eab6f4e0ac
Yanivba18 <47143949+Yanivba18@users.noreply.github.com>
2019-12-03 17:29:50 +0200

    Added remove and add by action functionality

--- js/gridBuilder.js
+++ js/gridBuilder.js
@@ -43,13 +43,17 @@
                 if (r < 3) {
                     for (let c = 0; c < 3; c++) {
                         let treeLeaf = document.getElementById(`${startRow}X${startCol}`);
-                        treeLeaf.classList.add("treeLeaf")
+                        treeLeaf.classList.add("treeLeaf");
+                        treeLeaf.setAttribute("tileType", "treeLeaf");
+                        treeLeaf.setAttribute("action", "axe");
                         startCol++;
                     }
                 }
                 else {
                     let treeTrunk = document.getElementById(`${startRow}X${startCol + 1}`);
-                    treeTrunk.classList.add("treeTrunk")
+                    treeTrunk.classList.add("treeTrunk");
+                    treeTrunk.setAttribute("tileType", "treeTrunk");
+                    treeTrunk.setAttribute("action", "axe");
                 }
                 startCol = treeStartCol;
                 startRow++;
@@ -63,8 +67,12 @@
                     let groundDiv = document.getElementById(`${r}X${c}`);
                     if (r == (this.totalRows - 1)) {
                         groundDiv.classList.add("lava");
+                        groundDiv.setAttribute("tileType", "lava");
+                        groundDiv.setAttribute("action", "eraser");
                     }
                     groundDiv.classList.add("ground");
+                    groundDiv.setAttribute("tileType", "ground");
+                    groundDiv.setAttribute("action", "shovel");
                 }
             }
         }
@@ -78,6 +86,8 @@
                     if ((currentRow == 1 || this.hasGroundBelow(r, c)) && chance > 0.5) {
                         let groundDiv = document.getElementById(`${r}X${c}`);
                         groundDiv.classList.add("hill");
+                        groundDiv.setAttribute("tileType", "hill")
+                        groundDiv.setAttribute("action", "shovel")
                     }
 
                 }
@@ -95,21 +105,7 @@
             }
         }
 
-        createToolBar() {
-            console.log("Tool bar not implemented yet")
-            //Need to implement the tool bar with 4 tools here
-            //Each one needs an onclick which changes gameUI.currentAction = chosenAction which is one of: 
-            // 1. "axe"
-            // 2. "shovel"
-            // 3. "pickaxe"
-            // 4. "eraser"
-        }
-
-        showMinedTiles() {
-            console.log("not implemented yet")
-            //Need to implement here a way to show the mined tiles save in gameUI.minedTiles object.
-            //The minedTiles object is build in a way of key : value. where key is the class of the mined tile and value is the amount
-        }
+        
     }
 
     class Tile {
@@ -129,7 +125,8 @@
     }
 
     let gameUI = {
-        currentAction: "eraser",
+        currentAction: "shovel",
+        currentTile: null,
         minedTiles: {},
     };
 
@@ -137,32 +134,40 @@
 
     gameUI.handleAction = (e) => {
         console.log(gameUI.currentAction + " " + e.target.classList)
-        if (e.target.classList == "regular-div") {
+        if (e.target.classList == "regular-div" && gameUI.currentAction == null) {
             console.log("Nothing in this div but sky");
+            e.target.classList.add(gameUI.currentTile);
             return;
         }
         let minedTile;
-        switch (gameUI.currentAction) {
-            case "axe":
+        if (gameUI.currentAction == "eraser" || e.target.getAttribute("action") == gameUI.currentAction) {
+            minedTile = e.target.getAttribute("tileType");
+            console.log("Mined tile: " + minedTile);            
+            gameUI.minedTiles[minedTile] ? gameUI.minedTiles[minedTile]++ : gameUI.minedTiles[minedTile] = 1;
 
-                break;
-            case "shovel":
-                break;
-            case "pickaxe":
-                break;
-            case "eraser":
-                minedTile = e.target.classList[1];
-                console.log("Mined tile: " + minedTile);
-                if (gameUI.minedTiles[minedTile]) {
-                    gameUI.minedTiles[minedTile]++;
-                } else {
-                    gameUI.minedTiles[minedTile] = 1;
-                }
-                console.log(gameUI.minedTiles);
-                e.target.classList = "regular-div";
-                break;
-        }
+            console.log(gameUI.minedTiles);
+            e.target.classList = "regular-div";
+            gameUI.showMinedTiles();
+        }        
     };
+
+
+    gameUI.createToolBar = () => {
+        //TODO - Zohar
+        //Need to implement the tool bar with 4 tools here
+        //Each one needs an onclick which changes gameUI.currentAction = chosenAction which is one of: 
+        // 1. "axe"
+        // 2. "shovel"
+        // 3. "pickaxe"
+        // 4. "eraser"
+    }
+
+    gameUI.showMinedTiles = () => {
+        //TODO - Zohar
+        //Need to implement here a way to show the mined tiles save in gameUI.minedTiles object.
+        //The minedTiles object is build in a way of key : value. where key is the class of the mined tile and value is the amount
+        //on click on mined tile - change gameUI.action to tileType
+    }
 
     function main() {
         let newGameUI = new GameGrid(40);
@@ -172,6 +177,8 @@
         // console.log(newGameUI.totalRows)
         newGameUI.createHills();
         newGameUI.createTree();
+        //newGameUI.createRocks();
+        //gameUI.createToolBar();
     }
 
     main();
