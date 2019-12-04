654a7b33d3bd746015ddd7b2ee4c64a1e6dc0616
Yanivba18 <47143949+Yanivba18@users.noreply.github.com>
2019-12-04 10:49:33 +0200

    Added create rock function

--- js/gridBuilder.js
+++ js/gridBuilder.js
@@ -82,8 +82,8 @@
             for (let r = (this.groundStartRow - 1); r > this.groundStartRow - 3; r--) {
                 //console.log(r)
                 for (let c = 0; c < this.totalCols; c++) {
-                    let chance = Math.random();
-                    if ((currentRow == 1 || this.hasGroundBelow(r, c)) && chance > 0.5) {
+                    let chance = Math.random() > 0.5;
+                    if ((currentRow == 1 || this.hasGroundBelow(r, c)) && chance) {
                         let groundDiv = document.getElementById(`${r}X${c}`);
                         groundDiv.classList.add("hill");
                         groundDiv.setAttribute("tileType", "hill")
@@ -95,148 +95,168 @@
             }
         }
 
-        hasGroundBelow(r, c) {
-            let below = document.getElementById(`${r + 1}X${c}`);
+        createRock() {
+            let currentRow = 1;
+            for (let r = (this.groundStartRow - 1); r > this.groundStartRow - 4; r--) {
+                for (let c = 0; c < this.totalCols; c++) {
+                    let chance = Math.random() > 0.5;
+                    let groundDiv = document.getElementById(`${r}X${c}`);
+                    if ((currentRow == 1 || this.hasGroundBelow(r, c)) && chance && groundDiv.className == "regular-div") {
+                        groundDiv.classList.add("rock");
+                        groundDiv.setAttribute("tileType", "rock");
+                        groundDiv.setAttribute("action", "pickaxe");
+                    }
 
-            if (below.classList.contains("hill") || below.classList.contains("ground")) {
-                return true;
-            } else {
-                return false;
+                }
+                currentRow++;
             }
         }
+    
 
-        
+
+    hasGroundBelow(r, c) {
+        let below = document.getElementById(`${r + 1}X${c}`);
+
+        if (below.classList.contains("hill") || below.classList.contains("ground") || below.classList.contains("rock")) {
+            return true;
+        } else {
+            return false;
+        }
     }
+
+
+}
 
     class Tile {
-        constructor(row, col) {
-            this.row = row;
-            this.col = col;
-            this.div = undefined;
-        }
-
-        getDiv() {
-            this.div = document.createElement("div");
-            this.div.classList.add("regular-div");
-            this.div.setAttribute("id", `${this.row}X${this.col}`);
-            this.div.addEventListener("click", function (e) { gameUI.handleAction(e) })
-            return this.div;
-        }
+    constructor(row, col) {
+        this.row = row;
+        this.col = col;
+        this.div = undefined;
     }
 
-    let gameUI = {
-        currentAction: "shovel",
-        currentTile: null,
-        minedTiles: {},
+    getDiv() {
+        this.div = document.createElement("div");
+        this.div.classList.add("regular-div");
+        this.div.setAttribute("id", `${this.row}X${this.col}`);
+        this.div.addEventListener("click", function (e) { gameUI.handleAction(e) })
+        return this.div;
+    }
+}
+
+let gameUI = {
+    currentAction: "shovel",
+    currentTile: null,
+    minedTiles: {},
+};
+
+
+
+gameUI.handleAction = (e) => {
+    //console.log(gameUI.currentAction + " " + e.target.classList)
+    if (e.target.classList == "regular-div" && gameUI.currentAction == null) {
+        console.log("Nothing in this div but sky");
+        e.target.classList.add(gameUI.currentTile);
+        return;
+    }
+    let minedTile;
+    if (gameUI.currentAction == "eraser" || e.target.getAttribute("action") == gameUI.currentAction) {
+        minedTile = e.target.getAttribute("tileType");
+        //console.log("Mined tile: " + minedTile);            
+        gameUI.minedTiles[minedTile] ? gameUI.minedTiles[minedTile]++ : gameUI.minedTiles[minedTile] = 1;
+
+
+        e.target.classList = "regular-div";
+        gameUI.showMinedTiles();
+    }
+};
+
+
+
+gameUI.createSideBar = () => {
+
+    //TODO - Zohar
+    //Need to implement the tool bar with 4 tools here
+    //Each one needs an onclick which changes gameUI.currentAction = chosenAction which is one of: 
+    // on click make gameUI.currentTile = null 
+    // 1. "axe"
+    // 2. "shovel"
+    // 3. "pickaxe"
+    // 4. "eraser"
+    let sidebar = document.createElement('div');
+    sidebar.setAttribute('id', 'sidebar');
+    document.body.prepend(sidebar);
+
+    let tools = ["axe", "shovel", "pickaxe", "eraser"]
+    let toolbar = document.createElement('div');
+    let minedTilesDiv = document.createElement('div');
+    toolbar.setAttribute('id', 'toolbar');
+    minedTilesDiv.setAttribute('id', 'minedTilesDiv');
+    for (let i = 0; i < 4; i++) {
+        toolbar.append(document.createElement('div'));
+        toolbar.getElementsByTagName('div')[i].setAttribute('class', 'tool');
+        toolbar.getElementsByTagName('div')[i].setAttribute('id', tools[i]);
+        toolbar.getElementsByTagName('div')[i].addEventListener('click', () => {
+            gameUI.currentTile = null;
+            gameUI.currentAction = event.target.id;
+            document.getElementById('main-container').className = '';
+            document.getElementById('main-container').classList.add(tools[i]);
+            for (let i = 0; i < document.getElementsByClassName('tool').length; i++) {
+                document.getElementsByClassName('tool')[i].classList.remove('selected')
+            }
+            event.target.classList.add('selected')
+        })
     };
+    document.getElementById('sidebar').append(toolbar);
+    document.getElementById('sidebar').append(minedTilesDiv);
 
+    ///////////////////////////// 
 
-
-    gameUI.handleAction = (e) => {
-        //console.log(gameUI.currentAction + " " + e.target.classList)
-        if (e.target.classList == "regular-div" && gameUI.currentAction == null) {
-            console.log("Nothing in this div but sky");
-            e.target.classList.add(gameUI.currentTile);
-            return;
-        }
-        let minedTile;
-        if (gameUI.currentAction == "eraser" || e.target.getAttribute("action") == gameUI.currentAction) {
-            minedTile = e.target.getAttribute("tileType");
-            //console.log("Mined tile: " + minedTile);            
-            gameUI.minedTiles[minedTile] ? gameUI.minedTiles[minedTile]++ : gameUI.minedTiles[minedTile] = 1;
-
-            
-            e.target.classList = "regular-div";
-            gameUI.showMinedTiles();
-        }        
-    };
-
-
-
-    gameUI.createSideBar = () => {
-        
-        //TODO - Zohar
-        //Need to implement the tool bar with 4 tools here
-        //Each one needs an onclick which changes gameUI.currentAction = chosenAction which is one of: 
-        // on click make gameUI.currentTile = null 
-        // 1. "axe"
-        // 2. "shovel"
-        // 3. "pickaxe"
-        // 4. "eraser"
-        let sidebar = document.createElement('div');
-        sidebar.setAttribute('id', 'sidebar');       
-        document.body.prepend(sidebar);
-
-        let tools = ["axe", "shovel" , "pickaxe", "eraser"]
-        let toolbar = document.createElement('div');
-        let minedTilesDiv = document.createElement('div');
-        toolbar.setAttribute('id', 'toolbar');
-        minedTilesDiv.setAttribute('id', 'minedTilesDiv');
-        for(let i = 0 ; i<4 ; i++){
-            toolbar.append(document.createElement('div'));
-            toolbar.getElementsByTagName('div')[i].setAttribute('class', 'tool');
-            toolbar.getElementsByTagName('div')[i].setAttribute('id', tools[i]);
-            toolbar.getElementsByTagName('div')[i].addEventListener('click',()=>{
-                gameUI.currentTile = null;
-                gameUI.currentAction = event.target.id;
-                document.getElementById('main-container').className = '';
-                document.getElementById('main-container').classList.add(tools[i]);
-                for (let i = 0 ;i < document.getElementsByClassName('tool').length ; i++){
-                    document.getElementsByClassName('tool')[i].classList.remove('selected')
-                }
-                event.target.classList.add('selected')
-            })
-        };
-        document.getElementById('sidebar').append(toolbar);
-        document.getElementById('sidebar').append(minedTilesDiv);
-
-        ///////////////////////////// 
-
-        let tileTypes = ['lava', 'ground', 'grass' , 'treeTrunk', 'treeLeaf', 'rock']
-        minedTilesDiv = document.getElementById('minedTilesDiv');
-        for (let i in tileTypes){
-            minedTilesDiv.append(document.createElement('div'));
-            minedTilesDiv.getElementsByTagName('div')[i].classList.add('minedTileDiv');
-            minedTilesDiv.getElementsByTagName('div')[i].setAttribute('id', tileTypes[i]);
-            //minedTilesDiv.getElementsByTagName('div')[i].append(document.createElement('div'))
-        }
-
-        
-
-    
+    let tileTypes = ['lava', 'ground', 'grass', 'treeTrunk', 'treeLeaf', 'rock']
+    minedTilesDiv = document.getElementById('minedTilesDiv');
+    for (let i in tileTypes) {
+        minedTilesDiv.append(document.createElement('div'));
+        minedTilesDiv.getElementsByTagName('div')[i].classList.add('minedTileDiv');
+        minedTilesDiv.getElementsByTagName('div')[i].setAttribute('id', tileTypes[i]);
+        //minedTilesDiv.getElementsByTagName('div')[i].append(document.createElement('div'))
     }
 
-    gameUI.showMinedTiles = () => {
-        
-        
-            
-        
-        
-        // for (let tile of Object.keys(gameUI.minedTiles)){
-
-        //     minedTilesDiv.append(document.createElement('div'));
-        // }
 
 
-        //TODO - Zohar
-        //Need to implement here a way to show the mined tiles save in gameUI.minedTiles object.
-        //The minedTiles object is build in a way of key : value. where key is the class of the mined tile and value is the amount
-        //on click on mined tile - change gameUI.action to class of the tile
-        //  on click gameUI.currentAction = null;
-        
-    }
 
-    function main() {
-        let newGameUI = new GameGrid(40);
-        newGameUI.buildGrid();
-        newGameUI.createGround();
-        // console.log(newGameUI.groundStartRow)
-        // console.log(newGameUI.totalRows)
-        newGameUI.createHills();
-        newGameUI.createTree();
-        //newGameUI.createRocks();
-        gameUI.createSideBar();
-    }
+}
 
-    main();
-})(window)
+gameUI.showMinedTiles = () => {
+
+
+
+
+
+    // for (let tile of Object.keys(gameUI.minedTiles)){
+
+    //     minedTilesDiv.append(document.createElement('div'));
+    // }
+
+
+    //TODO - Zohar
+    //Need to implement here a way to show the mined tiles save in gameUI.minedTiles object.
+    //The minedTiles object is build in a way of key : value. where key is the class of the mined tile and value is the amount
+    //on click on mined tile - change gameUI.action to class of the tile
+    //  on click gameUI.currentAction = null;
+
+}
+
+function main() {
+    let newGameUI = new GameGrid(40);
+    newGameUI.buildGrid();
+    newGameUI.createGround();
+    // console.log(newGameUI.groundStartRow)
+    // console.log(newGameUI.totalRows)
+    newGameUI.createHills();
+    newGameUI.createTree();
+    newGameUI.createRock();
+    //newGameUI.createRocks();
+    gameUI.createSideBar();
+}
+
+main();
+}) (window)
